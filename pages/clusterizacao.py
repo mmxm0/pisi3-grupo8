5fc183e50691f43e2f8a4028f2da64410947e5c6
Matheus Lima <matheuslima225@outlook.com>
2024-08-08 22:10:50 -0300

    método cotovelo

---
+++ pages/clusterizacao.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+from utils import read_df
+from sklearn.cluster import KMeans
+import plotly.express as px
+
+st.set_page_config(page_title="Clusterização", layout="wide")
+
+def main():
+    st.title("Análise de Clusterização - Elbow Method")
+
+    # Carregar os dados normalizados, padronizados e limpos
+    normalized_data_name = 'normalized_ACC_INTAKES_OUTCOMES'
+    scaled_data_name = 'scaled_ACC_INTAKES_OUTCOMES'
+    cleaned_data_name = 'data_cleaned'
+    data_normalized = read_df(normalized_data_name, extension='parquet')
+    data_scaled = read_df(scaled_data_name, extension='parquet')
+    data_cleaned = read_df(cleaned_data_name, extension='parquet')
+
+    # Selecionar características relevantes para a clusterização
+    features = ['age_upon_intake_(days)', 'time_in_shelter_days', 'dob_year', 'intake_hour', 'outcome_year']
+
+    # Função para calcular o Elbow
+    def calculate_elbow(data, features):
+        sse = []
+        k_values = range(1, 11)  # Testar de 1 a 10 clusters
+        for k in k_values:
+            kmeans = KMeans(n_clusters=k, random_state=42)
+            kmeans.fit(data[features])
+            sse.append(kmeans.inertia_)
+        return k_values, sse
+
+    # Elbow para os dados limpos
+    k_values_cleaned, sse_cleaned = calculate_elbow(data_cleaned, features)
+    fig_cleaned = px.line(x=k_values_cleaned, y=sse_cleaned, markers=True, title='Método Elbow para Dados Limpos',
+                          labels={'x': 'Número de Clusters', 'y': 'Soma dos Quadrados das Distâncias'})
+    st.plotly_chart(fig_cleaned)
+
+    # Elbow para os dados normalizados
+    k_values_norm, sse_norm = calculate_elbow(data_normalized, features)
+    fig_norm = px.line(x=k_values_norm, y=sse_norm, markers=True, title='Método Elbow para Dados Normalizados',
+                       labels={'x': 'Número de Clusters', 'y': 'Soma dos Quadrados das Distâncias'})
+    st.plotly_chart(fig_norm)
+
+    # Elbow para os dados padronizados
+    k_values_scaled, sse_scaled = calculate_elbow(data_scaled, features)
+    fig_scaled = px.line(x=k_values_scaled, y=sse_scaled, markers=True, title='Método Elbow para Dados Padronizados',
+                         labels={'x': 'Número de Clusters', 'y': 'Soma dos Quadrados das Distâncias'})
+    st.plotly_chart(fig_scaled)
+
+if _name_ == "_main_":
+    main()
--- pages/preprocess.py
+++ pages/preprocess.py
@@ -92,14 +92,14 @@ def scale_numeric_data(data):
     numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
     data_scaled = data.copy()
     data_scaled[numeric_cols] = scaler.fit_transform(data_scaled[numeric_cols])
-    return data, data_scaled, numeric_cols
+    return data_scaled, numeric_cols
 
 def normalize_numeric_data(data):
     scaler = MinMaxScaler()
     numeric_cols = data.select_dtypes(include=['float64', 'int64']).columns
     data_normalized = data.copy()
     data_normalized[numeric_cols] = scaler.fit_transform(data_normalized[numeric_cols])
-    return data, data_normalized, numeric_cols
+    return data_normalized, numeric_cols
 
 def apply_label_encoding(data):
     # Instanciar o LabelEncoder
@@ -116,7 +116,7 @@ def apply_label_encoding(data):
         data[f'{col}_encoded'] = le.fit_transform(data[col])
         
         # Exibir comparação antes e depois
-        st.markdown(f"**{col}**", unsafe_allow_html=True)
+        st.markdown(f"*{col}*", unsafe_allow_html=True)
         comparison_df = pd.DataFrame({
             'Original': data[col].head(10),
             'Encoded': data[f'{col}_encoded'].head(10)
@@ -150,7 +150,7 @@ def apply_and_display_one_hot_encoding(data):
         before = data[col].head(10).to_frame()  # Exibir apenas as 10 primeiras linhas para comparação
         after = data_encoded.filter(like=col).head(10)
         comparison_df = pd.concat([before, after], axis=1)
-        st.write(f"**Coluna: {col}**")
+        st.write(f"*Coluna: {col}*")
         st.write(comparison_df)
     
     return data_encoded, categorical_columns
@@ -198,43 +198,49 @@ def main():
     if outlier_columns:
         st.write(f"Foi identificado outliers nas seguintes colunas: {', '.join(outlier_columns)}")
         plot_outliers(data, outlier_columns)
+        # Remover outliers
+        data = remove_outliers(data, outlier_columns)
+        st.write(f"Outliers removidos. Número de registros restantes: {len(data)}")
     else:
         st.write("Não foram identificados outliers nas colunas numéricas.")
+
+    #Salvar o dataset original limpo como data_cleaned
+    data.to_parquet('data/data_cleaned.parquet')
     
     # Escalonamento de Dados Numéricos
     st.markdown("<h2>Escalonamento de dados:</h2>", unsafe_allow_html=True)
     st.write("""
         
-        1. **Padronização (Standard Scaling):**
+        1. *Padronização (Standard Scaling):*
            - Transforma os dados para que tenham média zero e desvio padrão igual a um.
            - É útil para algoritmos que assumem que os dados são distribuídos em uma forma normal (gaussiana).
         
-        2. **Normalização (Min-Max Scaling):**
+        2. *Normalização (Min-Max Scaling):*
            - Transforma os dados para que todos os valores estejam dentro do intervalo [0, 1].
            - É útil para algoritmos que não fazem suposições sobre a distribuição dos dados, como redes neurais e métodos de distância (por exemplo, k-NN).
         
-
     ### Abaixo estão as tabelas mostrando como as colunas foram alteradas antes e depois do escalonamento (padronização e normalização):
     """)
-    
-    data_before_scale, data_after_scale, numeric_cols_scale = scale_numeric_data(data)
-    data_before_norm, data_after_norm, numeric_cols_norm = normalize_numeric_data(data)
+
+    # Padronizar e Normalizar os dados
+    data_scaled, numeric_cols_scale = scale_numeric_data(data)
+    data_normalized, numeric_cols_norm = normalize_numeric_data(data)
     
     for col in numeric_cols_scale:
-        if not data_before_scale[col].equals(data_after_scale[col]):
-            st.write(f"**{col}** (Padronizado)")
+        if not data_scaled[col].equals(data_normalized[col]):
+            st.write(f"*{col}* (Padronizado)")
             comparison_df = pd.DataFrame({
-                'Antes': data_before_scale[col].head(8),
-                'Depois (Padronizado)': data_after_scale[col].head(8)
+                'Antes': data[col].head(8),
+                'Depois (Padronizado)': data_scaled[col].head(8)
             })
             st.write(comparison_df)
     
     for col in numeric_cols_norm:
-        if not data_before_norm[col].equals(data_after_norm[col]):
-            st.write(f"**{col}** (Normalizado)")
+        if not data_scaled[col].equals(data_normalized[col]):
+            st.write(f"*{col}* (Normalizado)")
             comparison_df = pd.DataFrame({
-                'Antes': data_before_norm[col].head(8),
-                'Depois (Normalizado)': data_after_norm[col].head(8)
+                'Antes': data[col].head(8),
+                'Depois (Normalizado)': data_normalized[col].head(8)
             })
             st.write(comparison_df)
     
@@ -242,23 +248,21 @@ def main():
     st.markdown("<h2>Codificação dos dados:</h2>", unsafe_allow_html=True)
     st.write("""
         ### One-Hot Encoding
-        Utilizamos One-Hot Encoding para colunas categóricas nominais onde não há uma ordem implícita entre os valores, como `animal_type`, `outcome_type`, `sex_upon_outcome`, `sex_upon_intake`, `intake_condition` e `intake_type`. Esta técnica cria uma nova coluna binária para cada valor único da categoria, permitindo que o modelo trate cada valor de maneira igual.
+        Utilizamos One-Hot Encoding para colunas categóricas nominais onde não há uma ordem implícita entre os valores, como animal_type, outcome_type, sex_upon_outcome, sex_upon_intake, intake_condition e intake_type. Esta técnica cria uma nova coluna binária para cada valor único da categoria, permitindo que o modelo trate cada valor de maneira igual.
 
         ### Label Encoding
-        Utilizamos Label Encoding para colunas categóricas ordinais onde há uma ordem implícita entre os valores, como `age_upon_outcome_age_group`, `outcome_subtype`, `outcome_weekday`, `outcome_number`, `intake_weekday` e `intake_number`. Esta técnica converte os valores categóricos em valores numéricos que refletem a ordem implícita entre eles.
+        Utilizamos Label Encoding para colunas categóricas ordinais onde há uma ordem implícita entre os valores, como age_upon_outcome_age_group, outcome_subtype, outcome_weekday, outcome_number, intake_weekday e intake_number. Esta técnica converte os valores categóricos em valores numéricos que refletem a ordem implícita entre eles.
     """)
 
     # Aplicar Label Encoding nas colunas específicas
-    data, columns_to_encode = apply_label_encoding(data)
+    data_encoded, columns_to_encode = apply_label_encoding(data)
 
     # Aplicar One-Hot Encoding e Mostrar Comparação
-    data_encoded, categorical_columns = apply_and_display_one_hot_encoding(data)
+    data_encoded, categorical_columns = apply_and_display_one_hot_encoding(data_encoded)
     
     # Criar os datasets finais
-    columns_to_drop = numeric_cols_scale.tolist() + numeric_cols_norm.tolist() + columns_to_encode + categorical_columns
-
-    data_final_scaled = data_encoded.drop(columns=columns_to_drop, errors='ignore').join(data_after_scale[numeric_cols_scale])
-    data_final_normalized = data_encoded.drop(columns=columns_to_drop, errors='ignore').join(data_after_norm[numeric_cols_norm])
+    data_final_scaled = data_encoded.drop(columns=numeric_cols_scale, errors='ignore').join(data_scaled[numeric_cols_scale])
+    data_final_normalized = data_encoded.drop(columns=numeric_cols_norm, errors='ignore').join(data_normalized[numeric_cols_norm])
 
     # Exibir o dataset final padronizado
     st.markdown("<h2>Dataset final padronizado após pré-processamento:</h2>", unsafe_allow_html=True)
@@ -287,6 +291,5 @@ def main():
     data_final_scaled.to_parquet('data/scaled_ACC_INTAKES_OUTCOMES.parquet')
     data_final_normalized.to_parquet('data/normalized_ACC_INTAKES_OUTCOMES.parquet')
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     main()
-
