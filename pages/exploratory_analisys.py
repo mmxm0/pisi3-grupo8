e34ed8cbecab204bb6c95349fcfe35de6c8f03de
Matheus Lima <matheuslima225@outlook.com>
2024-08-18 23:37:22 -0300

    update exploratory analysis

--- pages/exploratory_analisys.py
+++ pages/exploratory_analisys.py
@@ -9,34 +9,59 @@ def build_page():
     build_body()
 
 def build_header():
-    text = '<h1>Análises Exploratórias</h1>' + \
-           '<p>Esta página apresenta alguns gráficos a partir da base de dados fornecida pelo Centro Animal de Austin. ' + \
-           '(https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-intakes-and-outcomes?select=aac_intakes_outcomes.csv).</p>'
+    text ='<h1>Análises Exploratórias</h1>'+\
+    '<p>Esta página apresenta alguns gráficos a partir da base de dados fornecida pelo Centro Animal de Austin. '+\
+    '(https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-intakes-and-outcomes?select=aac_intakes_outcomes.csv).</p>'
     st.write(text, unsafe_allow_html=True)
 
 def build_body():
     df = read_ACC_df()
-    st.markdown('<h2>Gráficos relativos à idade do animal no momento de saída do abrigo</h2>', unsafe_allow_html=True)
     grafico_barra(df)
     grafico_boxplot(df)
     grafico_barras(df)
     grafico_histograma(df)
 
 def grafico_barra(df):
+    st.write('**Gráfico relativos à idade do animal no momento de saída do abrigo**')
     age_counts = df['age_upon_outcome'].value_counts().reset_index()
     age_counts.columns = ['Idade', 'Número de Animais']
-    bar_fig = px.bar(age_counts, x='Idade', y='Número de Animais', labels={'Idade': 'Idade no Momento da Saída', 'Número de Animais': 'Número de Animais'})
+
+    limite_relevancia = 200 
+    age_counts_filtrado = age_counts[age_counts['Número de Animais'] > limite_relevancia]
+
+    ordem_idades = ['1 month', '2 months', '3 months', '4 months', '5 months', '6 months', '7 months', '8 months', '9 months', '10 months', '11 months', '12 months']
+    ordem_idades = [idade for idade in ordem_idades if idade in age_counts_filtrado['Idade'].values]
+
+    idades_selecionadas = st.multiselect(
+        'Selecione as Idades que deseja analisar:',
+        options=ordem_idades, default=ordem_idades)
+    
+    age_counts_filtrado = age_counts_filtrado[age_counts_filtrado['Idade'].isin(idades_selecionadas)]
+    age_counts_filtrado['Idade'] = pd.Categorical(age_counts_filtrado['Idade'], categories=ordem_idades, ordered=True)
+    age_counts_filtrado = age_counts_filtrado.sort_values('Idade')
+
+    bar_fig = px.bar(
+        age_counts_filtrado, 
+        x='Número de Animais', 
+        y='Idade', 
+        orientation='h', 
+        labels={'Idade': 'Idade no Momento da Saída', 'Número de Animais': 'Número de Animais'})
+
     st.plotly_chart(bar_fig, use_container_width=True)
 
 def grafico_boxplot(df):
     st.write('**Boxplot de Idade (em anos)**')
-    selected_category = st.selectbox('Selecione a categoria para análise', options=['outcome_type', 'animal_type'], key='category_boxplot')
+    selected_category = st.selectbox('Selecione a categória para análise', options=['outcome_type', 'animal_type'], key='category_boxplot')
     box_fig = px.box(df, x=selected_category, y='age_upon_outcome_(years)', points="all", labels={selected_category: selected_category, 'age_upon_outcome_(years)': 'Idade (anos)'})
     st.plotly_chart(box_fig, use_container_width=True)
 
 def grafico_barras(df):
     st.write('**Gráfico de Barras: Comparação dos Tipos de Saída por Tipo de Animal**')
-    barras_data = df.groupby(['animal_type', 'outcome_type']).size().reset_index(name='count')
+    animal_tipo_selecionado = st.selectbox(
+        'Selecione o Tipo de Animal para análise:',
+        options=df['animal_type'].unique())
+    df_filtrado = df[df['animal_type'] == animal_tipo_selecionado]
+    barras_data = df_filtrado.groupby(['animal_type', 'outcome_type']).size().reset_index(name='count')
     bar_fig = px.bar(barras_data, 
                      x='animal_type', 
                      y='count', 
@@ -45,51 +70,25 @@ def grafico_barras(df):
                      labels={'animal_type': 'Tipo de Animal', 
                              'count': 'Quantidade', 
                              'outcome_type': 'Tipo de Saída'})
+    
     st.plotly_chart(bar_fig, use_container_width=True)
 
 def grafico_histograma(df):
     st.write('**Gráfico Histograma: Análise do Tipo de Saída por Condição de Entrada**')
+
+    tipo_saida_selecionado = st.selectbox(
+        'Selecione o Tipo de Saída para análise:',
+        options=df['outcome_type'].unique())
     
-    bar_fig = px.histogram(df, 
+    df_filtrado = df[df['outcome_type'] == tipo_saida_selecionado]
+    bar_fig = px.histogram(df_filtrado, 
                            x='intake_condition', 
                            color='outcome_type',
                            labels={'intake_condition': 'Condição na Entrada', 
                                    'outcome_type': 'Tipo de Saída',
                                    'count': 'Quantidade'},
-                           barmode='group')  # Agrupa as barras
+                           barmode='group')
     
     st.plotly_chart(bar_fig, use_container_width=True)
-
-    st.write('*Histograma relativo ao tempo total no abrigo por raça*')
-    
-    # Opções de filtro
-    opcao = st.selectbox('Selecione uma opção:', ('5 Raças Mais Adotadas', '5 Raças Menos Adotadas'))
-    
-    df_aux = df[['breed', 'time_in_shelter_days']].copy()
-    df_aux = df_aux.groupby('breed').agg(
-        qtd=('time_in_shelter_days', 'size'),
-        avg_time=('time_in_shelter_days', 'mean')
-    ).reset_index()
-
-    if opcao == '5 Raças Mais Adotadas':
-        df_filtered = df_aux.nlargest(5, 'qtd')
-    else:
-        df_filtered = df_aux.nsmallest(5, 'qtd')
-
-    fig = px.histogram(df_filtered, 
-                       x='breed', 
-                       y='avg_time', 
-                       nbins=5,
-                       labels={'breed': 'Raça', 
-                               'avg_time': 'Tempo Médio no Abrigo (dias)'})
-
-    fig.update_layout(
-        xaxis_title="Raça",
-        yaxis_title="Tempo Médio no Abrigo (dias)",
-        xaxis={'categoryorder':'total descending'},
-        showlegend=False
-    )
-
-    st.plotly_chart(fig, use_container_width=True)
 
 build_page()
